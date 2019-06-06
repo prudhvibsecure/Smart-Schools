fe2cadccc2a041ea6e29d628fb656751a97fb614
prudhvibsecure <prudhvi.bsecure@gmail.com>
2019-06-06 14:01:50 +0530

    6619

--- app/src/main/java/com/bsecure/scsm_mobile/modules/NonScholasticTeacher.java
+++ app/src/main/java/com/bsecure/scsm_mobile/modules/NonScholasticTeacher.java
@@ -2,6 +2,8 @@ package com.bsecure.scsm_mobile.modules;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.bsecure.scsm_mobile.R;
 
@@ -11,5 +13,22 @@ public class NonScholasticTeacher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_scholastic_teacher);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolset);
+        toolbar.setTitle("Non-scholastic Subjects");//Organization Head
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
